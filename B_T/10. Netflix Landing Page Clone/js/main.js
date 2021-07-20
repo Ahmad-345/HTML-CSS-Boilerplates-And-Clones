a9960273568a2c8a57802e8ae7724cdbb8a4f77f
Ahmad-345 <shadabsadiq1995@yahoo.com>
2021-07-20 17:07:16 +0500

    Netflic Homepage OLD

---
+++ B_T/10. Netflix Landing Page Clone/js/main.js
@@ -0,0 +1,29 @@
+// Variables
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+
+// Listen to tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+// Select tab content item
+function selectItem (e) {
+  // First remove the border from other tabs and also the content of the tab
+  removerBorder();
+  removeShow();
+  // Add border to current tab
+  this.classList.add('tab-border');
+  // Grab content of the tab from the DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class 
+  tabContentItem.classList.add('show');
+}
+
+function removerBorder() {
+  tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+  tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
